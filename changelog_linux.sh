102227de25f11e69c223fcf7661817cc6175f9d7
Siddharth Gelera <sidgroks@gmail.com>
2020-12-28 13:17:24 +0530

    ci: shift to bash scripts

--- changelog_linux.sh
+++ changelog_linux.sh
@@ -4,5 +4,4 @@ curl -s https://api.github.com/repos/barelyhuman/commitlog/releases/latest \
 | cut -d '"' -f 4 \
 | wget -qi -
 tar -xvzf commitlog-linux-amd64.tar.gz
-chmod +x commitlog 
 ./commitlog . > CHANGELOG.txt
